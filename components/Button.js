a21c55a941fe38a6ecd8c382d4f0fafd07cf84c4
Chaithanya Kota <chaithanya.kota007@gmail.com>
2022-03-04 08:06:39 +0530

    ok

--- components/Button.js
+++ components/Button.js
@@ -3,9 +3,9 @@ import React from "react";
 function Button(props) {
   return (
     <div>
-      <ul class="flex space-x-2">
+      <ul className="flex space-x-2">
         <li>
-          <a className="cursor-pointer flex flex-row justify-center items-center py-1.5 px-3 relative bg-white rounded-md text-black uppercase font-medium font-mono text-xs leading-4 font-500 bg-opacity-10 duration-500 hover:bg-opacity-60 tracking-widest shadow-sm">
+          <a className="cursor-pointer flex flex-row justify-center items-center py-1.5 px-3 relative bg-white rounded-md text-black uppercase font-medium font-mono text-xs leading-4 font-100 bg-opacity-10 duration-500 hover:bg-opacity-60 tracking-widest shadow-sm">
             {props.children}
           </a>
         </li>
